c631e16d5248fbd2ab70da510c5b521d95dc92a8
DevThiagoSantana <113400543+DevThiagoSantana@users.noreply.github.com>
2023-05-26 21:16:58 -0300

    commit Test

---
+++ src/test/java/tech/devinhouse/apiaviacao/controller/PassageiroControllerTest.java
@@ -0,0 +1,20 @@
+package tech.devinhouse.apiaviacao.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PassageiroControllerTest {
+
+    @Test
+    void listar() {
+    }
+
+    @Test
+    void consultarPeloCpf() {
+    }
+
+    @Test
+    void confirmacao() {
+    }
+}
